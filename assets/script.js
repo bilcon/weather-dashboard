245c1df99de493ae6dbe7b6c58d7b0c05b4439ea
Bill Morris <bilcon@swbell.net>
2021-10-28 17:45:44 -0500

    added recent city functions

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,36 @@ $(document).ready(function () {
     var day4 = moment().add(4, "days").format("l");
     var day5 = moment().add(5, "days").format("l");
   
-   //global variables
+    //global variables
     var city;
     var cities;
+
+    //function to load most recently searched city from local storage
+    function loadMostRecent() {
+        var lastSearch = localStorage.getItem("mostRecent");
+        if (lastSearch) {
+          city = lastSearch;
+          search();
+        } else {
+          city = "Seattle";
+          search();
+        }
+      }
+    
+      loadMostRecent()
+    
+    //function to load recently searched cities from local storage
+      function loadRecentCities() {
+        var recentCities = JSON.parse(localStorage.getItem("cities"));
+    
+        if (recentCities) {
+          cities = recentCities;
+        } else {
+          cities = [];
+        }
+      }
+    
+      loadRecentCities()
 
     $("#submit").on("click", (e) => {
         e.preventDefault();
@@ -43,7 +70,7 @@ $(document).ready(function () {
     //event handler for recently searched cities in table
     $(document).on("click", "td", (e) => {
         e.preventDefault();
-        let listedCity = $(e.target).text();
+        var listedCity = $(e.target).text();
         city = listedCity;
         search();
     });
