83f8a724a358ca887f465b2c6c9c60f0f046b896
Bill Morris <bilcon@swbell.net>
2021-10-28 15:01:16 -0500

    started java added variables and clear button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function () {
 
+    var NowMoment = moment().format("l");
+  
+    //adds days to moment for forecast
+    var day1 = moment().add(1, "days").format("l");
+    var day2 = moment().add(2, "days").format("l");
+    var day3 = moment().add(3, "days").format("l");
+    var day4 = moment().add(4, "days").format("l");
+    var day5 = moment().add(5, "days").format("l");
+  
+   //global variables
+    var city;
+    var cities;
+
+    $("#clr-btn").click(() => {
+        localStorage.removeItem("cities");
+        loadRecentCities();
+        listCities();
+    });
 });
